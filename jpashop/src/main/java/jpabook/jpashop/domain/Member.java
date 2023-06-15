b8ec0d4e98c41e1f5ebf8a2122e480490da4ba83
Junyoung <wnsdud12365@naver.com>
2023-06-15 20:57:03 +0900

    Feat : 양방향 연관관계 (객체 참조) 추가

--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter @Setter
@@ -17,4 +19,11 @@ public class Member {
     private String street;
     private String zipcode;
 
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+
+    public void addOrders(Order order){
+        orders.add(order);
+        order.setMember(this);
+    }
 }
